2949443a8a81c8dc1d872d46c736451d42e332b6
tobyret <toby.retallick@gmail.com>
2015-12-07 16:51:07 +0000

    Validation rule for unique numbers

---
+++ src/main/java/com/codurance/validation/UniqueNumbersRule.java
@@ -0,0 +1,14 @@
+package com.codurance.validation;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UniqueNumbersRule implements ValidationRule {
+    @Override
+    public boolean validate(List<Integer> numberSequence) {
+        Set<Integer> numberSequenceToSet = new HashSet<>(numberSequence);
+
+        return numberSequenceToSet.size() == numberSequence.size();
+    }
+}
---
+++ src/test/java/com/codurance/validation/NumbersAreUniqueRuleShould.java
@@ -0,0 +1,33 @@
+package com.codurance.validation;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class NumbersAreUniqueRuleShould {
+
+    private UniqueNumbersRule uniqueNumbersRule;
+
+    @Before
+    public void setUp() {
+        uniqueNumbersRule = new UniqueNumbersRule();
+    }
+
+    @Test
+    public void returnFalseIfNumbersAreNotUnique() {
+        List<Integer> listWithDuplicateNumbers = asList(1, 3, 3, 2, 4, 5);
+
+        assertThat(uniqueNumbersRule.validate(listWithDuplicateNumbers), is(false));
+    }
+
+    @Test
+    public void returnTrueIfNumbersAreUnique() {
+        List<Integer> listWithUniqueNumbers = asList(1,2,3,4,5);
+
+        assertThat(uniqueNumbersRule.validate(listWithUniqueNumbers), is(true));
+    }
+}

81c19d92588422f9432bdd70dbc7d3a93ca3eac5
tobyret <toby.retallick@gmail.com>
2015-12-08 11:45:50 +0000

    Add static imports

--- src/test/java/com/codurance/validator/rules/EvenNumbersAreSequentialRuleShould.java
+++ src/test/java/com/codurance/validator/rules/EvenNumbersAreSequentialRuleShould.java
@@ -1,11 +1,11 @@
 package com.codurance.validator.rules;
 
-import java.util.Arrays;
 import java.util.List;
 import com.codurance.housenumbers.HouseNumbers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -20,14 +20,14 @@ public class EvenNumbersAreSequentialRuleShould {
 
     @Test
     public void returnFalseIfOddNumbersAreNonSequential() {
-        List<Integer> nonSequentialEvenNumbersList = Arrays.asList(1, 3, 4, 2, 5);
+        List<Integer> nonSequentialEvenNumbersList = asList(1, 3, 4, 2, 5);
 
         assertThat(evenNumbersAreSequentialRule.validate(new HouseNumbers(nonSequentialEvenNumbersList)), is(false));
     }
 
     @Test
     public void returnTrueIfOddNumbersAreSequential() {
-        List<Integer> sequentialEvenNumbersList = Arrays.asList(1, 3, 2, 4, 5);
+        List<Integer> sequentialEvenNumbersList = asList(1, 3, 2, 4, 5);
 
         assertThat(evenNumbersAreSequentialRule.validate(new HouseNumbers(sequentialEvenNumbersList)), is(true));
     }
--- src/test/java/com/codurance/validator/rules/NumbersStartFromOneRuleShould.java
+++ src/test/java/com/codurance/validator/rules/NumbersStartFromOneRuleShould.java
@@ -1,11 +1,11 @@
 package com.codurance.validator.rules;
 
-import java.util.Arrays;
 import java.util.List;
 import com.codurance.housenumbers.HouseNumbers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -21,14 +21,14 @@ public class NumbersStartFromOneRuleShould {
 
     @Test
     public void returnTrueIfHouseNumberingStartsAtOne() {
-        List<Integer> correctNumberSequence = Arrays.asList(1, 3, 2, 5, 4);
+        List<Integer> correctNumberSequence = asList(1, 3, 2, 5, 4);
 
         assertThat(numberingStartsAtOneRule.validate(new HouseNumbers(correctNumberSequence)), is(true));
     }
 
     @Test
     public void returnFalseIfHouseNumberingDoesNotStartAtOne() {
-        List<Integer> incorrectNumberSequence = Arrays.asList(3, 2, 5, 4);
+        List<Integer> incorrectNumberSequence = asList(3, 2, 5, 4);
 
         assertThat(numberingStartsAtOneRule.validate(new HouseNumbers(incorrectNumberSequence)), is(false));
     }
